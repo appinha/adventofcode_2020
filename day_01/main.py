4c20b5bb15ee0c571a110c026635200ed24006c1
appinha <amanda_pinha@hotmail.com>
2020-12-08 15:51:50 -0300

    Ex.1: conformed to common_core

--- day_01/main.py
+++ day_01/main.py
@@ -1,40 +1,41 @@
-from termcolor import colored
+# Import common core
+import sys
+sys.path.append('../common_core')
+from common_core import core
+
+# Import requirements
+from numpy import prod
 
 
-def get_input():
-	'''
-		Open input file and split string into list of numbers.
-	'''
-	with open('input.txt', 'r') as f:
-		input_string = f.read()
-	return list(map(int, (input_string.split('\n'))))
+input_file = sys.argv[1]
+
+
+def ft_input_parser(raw_input):
+	return [int(line) for line in raw_input]
+
+
+def find_nbrs(data, qty):
+	# Loop through list of numbers n times for combinatorial analysis
+	for i in range(len(data)):
+		for j in range(i + 1, len(data)):
+			# PART ONE - Check if sum of two numbers is equal to 2020
+			if (qty == 2):
+				if (data[i] + data[j] == 2020):
+					return [data[i], data[j]]
+			# PART TWO - Check if sum of three numbers is equal to 2020
+			elif (qty == 3):
+				for k in range(j + 1, len(data)):
+					if (data[i] + data[j] + data[k] == 2020):
+						return [data[i], data[j], data[k]]
+
+
+def ft_part1(data):
+	return prod(find_nbrs(data, 2))
+
+
+def ft_part2(data):
+	return prod(find_nbrs(data, 3))
 
 
 if __name__ == '__main__':
-
-	part_one = colored("\nPart One:", 'magenta')
-	part_two = colored("\nPart Two:", 'magenta')
-
-	nbrs = get_input()
-
-	# Loop through list of numbers n times for combinatorial analysis
-	for i in range(len(nbrs)):
-		for j in range(i + 1, len(nbrs)):
-			# PART ONE - Check if sum of two numbers is equal to 2020
-			if (nbrs[i] + nbrs[j] == 2020):
-				# PART ONE - Get numbers
-				p1_nbr_1 = nbrs[i]
-				p1_nbr_2 = nbrs[j]
-			for k in range(j + 1, len(nbrs)):
-				# PART TWO - Check if sum of three numbers is equal to 2020
-				if (nbrs[i] + nbrs[j] + nbrs[k] == 2020):
-					# PART TWO - Get numbers
-					p2_nbr_1 = nbrs[i]
-					p2_nbr_2 = nbrs[j]
-					p2_nbr_3 = nbrs[k]
-
-	# Print numbers and its products
-	print(part_one, p1_nbr_1, "*", p1_nbr_2, "=", p1_nbr_1 * p1_nbr_2)
-	print(part_two, p2_nbr_1, "*", p2_nbr_2, "*", p2_nbr_3, "=", \
-			p1_nbr_1 * p1_nbr_2 * p2_nbr_3)
-	print()
+	core(input_file, ft_input_parser, ft_part1, ft_part2)
