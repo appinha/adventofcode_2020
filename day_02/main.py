a00b0d8f786a68465c29c332c2defa380940823b
appinha <amanda_pinha@hotmail.com>
2020-12-08 17:42:24 -0300

    Ex.2: conformed to common_core

--- day_02/main.py
+++ day_02/main.py
@@ -1,25 +1,21 @@
-from termcolor import colored
+# Import common core
+import sys
+sys.path.append('../common_core')
+from common_core import core
+
+# Import requirements
 import re
 
 
-def get_input():
-	'''
-		Open input file and split string into list of lines.
-	'''
-	with open('input.txt', 'r') as f:
-		input_string = f.read()
-	return list(map(str, (input_string.split('\n'))))
+input_file = sys.argv[1]
 
 
-def split_line_values(lines):
+def ft_input_parser(raw_input):
 	'''
-		Split with regex each line's values - two integers, a character and a
-		string - into sublists.
+		Split with regex each input line's values - two integers, a character
+		and a string - into sublists.
 	'''
-	pwds = []
-	for i in range(len(lines)):
-		pwds.append(re.split('-| |: ', lines[i]))
-	return pwds
+	return [re.split('-| |: ', line) for line in raw_input]
 
 
 # Values' indexes
@@ -29,48 +25,37 @@ CHAR = 2
 STR = 3
 
 
-def p1_check(pwd, ind_1, ind_2):
-	'''
-		Check if given password is valid according to Part One password policy.
-	'''
-	count = pwd[STR].count(pwd[CHAR])
-	if ind_1 <= count <= ind_2:
-		return True
-	return False
+def ft_part1(data):
+
+	def check(pwd, ind_1, ind_2):
+		''' Check if given password is valid according to password policy. '''
+		count = pwd[STR].count(pwd[CHAR])
+		if ind_1 <= count <= ind_2:
+			return True
+		return False
+
+	return len([pwd for pwd in data \
+				if check(pwd, int(pwd[IND_1]), int(pwd[IND_2]))])
 
 
-def p2_check(line, ind_1, ind_2):
-	'''
-		Check if given password is valid according to Part Two password policy.
-	'''
+def ft_part2(data):
 
-	def check(line, ind):
-		if ind < len(line[STR]):
-			if line[CHAR] == line[STR][ind]:
-				return 1
-		return 0
+	def count_checks(line, ind_1, ind_2):
+		''' Check if given password is valid according to password policy. '''
 
-	count = check(line, ind_1) + check(line, ind_2)
-	if count == 1:
-		return True
-	return False
+		def check(line, ind):
+			if ind < len(line[STR]):
+				if line[CHAR] == line[STR][ind]:
+					return 1
+			return 0
+
+		if (check(line, ind_1) + check(line, ind_2)) == 1:
+			return True
+		return False
+
+	return len([pwd for pwd in data \
+				if count_checks(pwd, int(pwd[IND_1]) - 1, int(pwd[IND_2]) - 1)])
 
 
 if __name__ == '__main__':
-
-	part_one = colored("\nPart One:", 'magenta')
-	part_two = colored("\nPart Two:", 'magenta')
-
-	pwds = split_line_values(get_input())
-
-	valid_pwds_1 = []
-	valid_pwds_2 = []
-	for i in range(len(pwds)):
-		if p1_check(pwds[i], int(pwds[i][IND_1]), int(pwds[i][IND_2])):
-			valid_pwds_1.append(pwds[i])
-		if p2_check(pwds[i], int(pwds[i][IND_1]) - 1, int(pwds[i][IND_2]) - 1):
-			valid_pwds_2.append(pwds[i])
-
-	print(part_one, colored("Valid passwords =", 'blue'), len(valid_pwds_1))
-	print(part_two, colored("Valid passwords =", 'blue'), len(valid_pwds_2))
-	print()
+	core(input_file, ft_input_parser, ft_part1, ft_part2)
