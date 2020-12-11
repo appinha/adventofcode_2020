c33fa02ebae2ab777d3102a0bbf39ee6acb5063f
appinha <amanda_pinha@hotmail.com>
2020-12-11 12:59:46 -0300

    Day 10: streamlined part 2, documented code

--- day_10/main.py
+++ day_10/main.py
@@ -3,16 +3,28 @@ import sys
 sys.path.append('../common_core')
 from common_core import core
 
+# Import requirements
+from numpy import prod
+
 
 input_file = sys.argv[1]
 
 
 def ft_input_parser(raw_input):
+	'''
+		This function parses the input, converting the given list of strings
+		(lines from input file) to a list of integers.
+	'''
 	data = [int(line) for line in raw_input]
 	return [0] + sorted(data) + [max(data) + 3]
 
 
 def ft_part1(data):
+	'''
+		This function counts how many deltas between numbers ("jolt difference")
+		of given sequence are equal to 1 and 3. The return is the product of
+		these counts.
+	'''
 	diffs = [0, 0]
 	for i in range(1, len(data)):
 		diff = data[i] - data[i - 1]
@@ -20,14 +32,26 @@ def ft_part1(data):
 			diffs[0] += 1
 		if diff == 3:
 			diffs[1] += 1
+	print(diffs)
 	return diffs[0] * diffs[1]
 
 
 def ft_part2(data):
+	'''
+		This function creates a string with the deltas between numbers of given
+		sequence ("131113113111133"), then splits it on the 3s. The resulting
+		list of subgroups of ones ([[1], [111], [11], [1111], []]) is pruned and
+		converted to a list of subgroups' quantity of 1s ([3, 2, 4]), with
+		length < 2 subgroups being discarded because they don't allow for any
+		combinations other than the subgroup itself. The quantity of 1s is thus
+		substituted for the number of possible combinations in each subgroup
+		([4, 2, 7]). The return is the combination of all subgroup combinations,
+		i.e. the product of these numbers.
+	'''
 	subgroups = ''.join(str(data[i + 1] - data[i]) for i in range(len(data) - 1)).split('3')
-	deltas = [len(ones) for ones in subgroups if len(ones) > 1]
-	sums = [sum(1 for n in deltas if n == i) for i in range(2, 5)]
-	return (2 ** sums[0]) * (4 ** sums[1]) * (7 ** sums[2])
+	qty_ones = [len(ones) for ones in subgroups if len(ones) > 1]
+	cmbs = [2, 4, 7]
+	return prod([cmbs[n - 2] for n in qty_ones])
 
 
 if __name__ == '__main__':
