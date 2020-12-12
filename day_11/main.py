1c68c981b4a77e3ce8ade4d660ea1f4d06e4fdbe
appinha <amanda_pinha@hotmail.com>
2020-12-12 19:11:54 -0300

    Day 11: corrected bug in part 1 function

--- day_11/main.py
+++ day_11/main.py
@@ -11,77 +11,99 @@ input_file = sys.argv[1]
 
 
 def ft_input_parser(raw_input):
-	data = dict.fromkeys(['o', '.', '#'], [])
-	data['map'] = np.matrix([list(raw_input[row]) for row in range(len(raw_input))])
+	'''
+		This function parses the input, converting the given list of strings
+		(lines from input file), which represents a seat map in form of a grid,
+		to a dictionary (function's return) with keys for storing seat positions
+		(tuples) according to seat's state:
+			- 'u': seats in unpermanent state;
+			- '.': floor (a permanent state);
+			- '#': permanentely occupied seats;
+			- 'L': permanentely empty seats;
+	'''
+	data = dict.fromkeys(['u', '.', '#'], [])
 	for row in range(len(raw_input)):
-		for col in range(len(raw_input[row])):
-			if raw_input[row][col] == '.':
+		for col, value in enumerate(raw_input[row]):
+			if value == '.':
 				data['.'] = data.get('.') + [(row, col)]
 			if raw_input[row][col] == 'L':
-				data['o'] = data.get('o') + [(row, col)]
+				data['u'] = data.get('u') + [(row, col)]
+	data['map'] = np.matrix([list(raw_input[row]) for row in range(len(raw_input))])
 	return data
-
-
-def print_dict(data):
-	for key in data:
-		print("•", key, "-", data[key])
 
 
 ROW = 0
 COL = 1
 
-def change_key(data, ls_pos, new):
+
+def get_map_size(data):
+	''' Get map size: (total rows, total cols). '''
+	return (data['map'].shape[ROW], data['map'].shape[COL])
+
+
+def get_adjacents(ls_pos, map_size):
+	''' Create a dictionary of adjacent seats (value) for all seats (keys). '''
+	rel_adj = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+	dc_adj = {}
 	for pos in ls_pos:
-		data['o'].remove(pos)
-		if new == '#':
-			data[new] = data.get(new) + [pos]
-			data['map'][pos] = new
+		tmp = [(a[ROW] + pos[ROW], a[COL] + pos[COL]) for a in rel_adj]
+		dc_adj[pos] = [t for t in tmp if 0 <= t[ROW] < map_size[ROW] and \
+										0 <= t[COL] < map_size[COL]]
+	return dc_adj
 
 
-def get_adj(data, pos):
-	map_len = data['map'].shape[COL]
-	adj = [[0, -1], [0, 1], [-1, 0], [1, 0], [-1, -1], [1, 1], [-1, 1], [1, -1]]
-	for a in adj:
-		a[ROW] += pos[ROW]
-		a[COL] += pos[COL]
-	return [tuple(a) for a in adj if 0 <= a[ROW] < map_len and 0 <= a[COL] < map_len]
-
-
-def get_occ(data, pos):
-	adj = get_adj(data, pos)
+def get_occ(data, adj, pos):
+	''' Get list of permanently occupied seats. '''
 	count = 0
 	for a in adj:
-		if a not in data['o']:
-			count += 1
-	if len(adj) < 8:
-		count += 8 - len(adj)
-	if count > 4:
-		return True
-	return False
+		count += 1 if a not in data['u'] else 0
+	count += (8 - len(adj)) if len(adj) < 8 else 0
+	return True if count > 4 else False
 
 
-def get_emp(data, pos):
-	adj = get_adj(data, pos)
+def get_emp(data, adj, pos):
+	''' Get list of permanently empty seats. '''
 	for a in adj:
 		if a in data['#']:
 			return True
 	return False
 
 
-def ft_part1(data):
-	print("Total seats:", len(data['o']))
+def update_state(data, ls_pos, state):
+	''' Update seat states from unpermanent to permanent. '''
+	for pos in ls_pos:
+		data['u'].remove(pos)
+		if state == '#':
+			data[state] = data.get(state) + [pos]
+			data['map'][pos] = state
+
+
+def seating_game_1(data):
+	'''
+		This function loops through seating rules, changing seat states from
+		unpermanent to permanent until no unpermanently stated seat remains.
+		The return is the quantity of permanently occupied seats.
+	'''
+	map_size = get_map_size(data)
+	dc_adj = get_adjacents(data['u'], map_size)
+	print("• Map size:", map_size[0], "x", map_size[1], \
+			"=", map_size[0] * map_size[1], "positions")
+	print("• Total seats:", len(data['u']))
 
 	i = 0
-	while data['o']:
-		occ = [pos for pos in data['o'] if get_occ(data, pos)]
-		change_key(data, occ, '#')
-		emp = [pos for pos in data['o'] if get_emp(data, pos)]
-		change_key(data, emp, 'L')
+	while data['u']:
+		occ = [pos for pos in data['u'] if get_occ(data, dc_adj[pos], pos)]
+		update_state(data, occ, '#')
+		emp = [pos for pos in data['u'] if get_emp(data, dc_adj[pos], pos)]
+		update_state(data, emp, 'L')
 		i += 1
-	print("inter:", i)
+	print("• Total interactions:", i, "\n")
 
-	#print_dict(data)
 	return len(data['#'])
+
+
+def ft_part1(data):
+	return seating_game_1(data)
 
 
 #def ft_part2(data):
