b4d87a7c42f5d84d87432f3ca8170bc501680828
appinha <amanda_pinha@hotmail.com>
2020-12-12 21:35:07 -0300

    Day 11: passed

--- day_11/main.py
+++ day_11/main.py
@@ -4,6 +4,7 @@ sys.path.append('../common_core')
 from common_core import core
 
 # Import requirements
+import copy
 import numpy as np
 
 
@@ -36,29 +37,54 @@ ROW = 0
 COL = 1
 
 
-def get_map_size(data):
-	''' Get map size: (total rows, total cols). '''
-	return (data['map'].shape[ROW], data['map'].shape[COL])
-
-
-def get_adjacents(ls_pos, map_size):
+def get_all_adjacents(data, map_size, in_sight=False):
 	''' Create a dictionary of adjacent seats (value) for all seats (keys). '''
-	rel_adj = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (1, 1), (-1, 1), (1, -1)]
+	def check_in_map(pos):
+		''' Check if position is inside seat map. '''
+		return True if 0 <= pos[ROW] < map_size[ROW] and \
+						0 <= pos[COL] < map_size[COL] else False
+
+	def get_imed_adj(pos, ls_rel_a):
+		''' Get imediatly adjacent seats (part 1 adjacent rule). '''
+		ls_adj = [(a[ROW] + pos[ROW], a[COL] + pos[COL]) for a in ls_rel_a]
+		return [a for a in ls_adj if check_in_map(a)]
+
+	def get_in_sight(data, pos, ls_rel_a):
+		''' Get seats in sight (part 2 adjacent rule). '''
+		i = 1
+		ls_adj = []
+		while ls_rel_a:
+			ls_to_remove = []
+			for rel_a in ls_rel_a:
+				abs_a = (i * rel_a[ROW] + pos[ROW], i * rel_a[COL] + pos[COL])
+				if check_in_map(abs_a) == False:
+					ls_to_remove.append(rel_a)
+				elif abs_a not in data['.']:
+					ls_adj.append(abs_a)
+					ls_to_remove.append(rel_a)
+			for to_remove in ls_to_remove:
+				ls_rel_a.remove(to_remove)
+			i += 1
+		return ls_adj
+
+	ls_rel_a = [(r, c) for r in range(-1, 2) for c in range(-1, 2) if not (r == 0 and c == 0)]
 	dc_adj = {}
-	for pos in ls_pos:
-		tmp = [(a[ROW] + pos[ROW], a[COL] + pos[COL]) for a in rel_adj]
-		dc_adj[pos] = [t for t in tmp if 0 <= t[ROW] < map_size[ROW] and \
-										0 <= t[COL] < map_size[COL]]
+	for pos in data['u']:
+		dc_adj[pos] = get_in_sight(data, pos, ls_rel_a[:]) if in_sight \
+						else get_imed_adj(pos, ls_rel_a[:])
 	return dc_adj
 
 
-def get_occ(data, adj, pos):
+def get_occ(data, adj, pos, part):
 	''' Get list of permanently occupied seats. '''
 	count = 0
 	for a in adj:
 		count += 1 if a not in data['u'] else 0
 	count += (8 - len(adj)) if len(adj) < 8 else 0
-	return True if count > 4 else False
+	if part == 1:
+		return True if count > 4 else False
+	if part == 2:
+		return True if count > 3 else False
 
 
 def get_emp(data, adj, pos):
@@ -78,39 +104,40 @@ def update_state(data, ls_pos, state):
 			data['map'][pos] = state
 
 
-def seating_game_1(data):
+def seating_game(data, part):
 	'''
 		This function loops through seating rules, changing seat states from
 		unpermanent to permanent until no unpermanently stated seat remains.
 		The return is the quantity of permanently occupied seats.
 	'''
-	map_size = get_map_size(data)
-	dc_adj = get_adjacents(data['u'], map_size)
-	print("• Map size:", map_size[0], "x", map_size[1], \
-			"=", map_size[0] * map_size[1], "positions")
-	print("• Total seats:", len(data['u']))
+	map_size = (data['map'].shape[ROW], data['map'].shape[COL])
+	if part == 1:
+		print("• Map size:", map_size[0], "x", map_size[1], \
+				"=", map_size[0] * map_size[1], "positions")
+		print("• Total seats:", len(data['u']))
+		dc_adj = get_all_adjacents(data, map_size)
+	if part == 2:
+		dc_adj = get_all_adjacents(data, map_size, in_sight=True)
 
 	i = 0
 	while data['u']:
-		occ = [pos for pos in data['u'] if get_occ(data, dc_adj[pos], pos)]
+		occ = [pos for pos in data['u'] if get_occ(data, dc_adj[pos], pos, part)]
 		update_state(data, occ, '#')
 		emp = [pos for pos in data['u'] if get_emp(data, dc_adj[pos], pos)]
 		update_state(data, emp, 'L')
 		i += 1
-	print("• Total interactions:", i, "\n")
+	print("• Total interactions:", i)
 
 	return len(data['#'])
 
 
 def ft_part1(data):
-	return seating_game_1(data)
+	return seating_game(copy.deepcopy(data), 1)
 
 
-#def ft_part2(data):
-
-#	return
+def ft_part2(data):
+	return seating_game(copy.deepcopy(data), 2)
 
 
 if __name__ == '__main__':
-	#core(input_file, ft_input_parser, ft_part1, ft_part2, delimiter='\n\n')
-	core(input_file, ft_input_parser, ft_part1, False)
+	core(input_file, ft_input_parser, ft_part1, ft_part2)
