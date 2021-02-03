d0915b9e1f4abeb86c6a9a35bd12f4a33b64d839
appinha <amanda_pinha@hotmail.com>
2021-02-03 07:00:01 -0300

    Day 01: refactored code, standardized naming and revised documentation

--- my_solutions/day_01/README.md
+++ my_solutions/day_01/README.md
@@ -1,4 +1,12 @@
 # Day 1: Report Repair
 
-## Puzzle instructions:
+## Summary
+
+📃 **Input:** expense report - i.e. list of integer numbers.
+
+⭐ **Part One:** find the 2 entries (numbers) that sum to 2020, then multiply them together.
+
+⭐ **Part Two:** find the 3 entries (numbers) that sum to 2020, then multiply them together.
+
+## Puzzle instructions
 https://adventofcode.com/2020/day/1
--- my_solutions/day_01/main.py
+++ my_solutions/day_01/main.py
@@ -4,38 +4,31 @@ sys.path.append('../common_core')
 from common_core import ft_core
 
 # Import requirements
-from numpy import prod
-
-
-input_file = sys.argv[1]
+import itertools as it
+import numpy as np
 
 
 def ft_input_parser(raw_input):
+	''' Convert input into list of numbers (integers). '''
 	return [int(line) for line in raw_input]
 
 
-def find_nbrs(data, qty):
-	# Loop through list of numbers n times for combinatorial analysis
-	for i in range(len(data)):
-		for j in range(i + 1, len(data)):
-			# PART ONE - Check if sum of two numbers is equal to 2020
-			if (qty == 2):
-				if (data[i] + data[j] == 2020):
-					return [data[i], data[j]]
-			# PART TWO - Check if sum of three numbers is equal to 2020
-			elif (qty == 3):
-				for k in range(j + 1, len(data)):
-					if (data[i] + data[j] + data[k] == 2020):
-						return [data[i], data[j], data[k]]
+def ft_find_nbrs(ls_nbrs, qty):
+	''' Find numbers in given list (ls_nbrs) whose sum is equal to 2020. '''
+	for nbrs in it.combinations(ls_nbrs, qty):
+		if sum(nbrs) == 2020:
+			return nbrs
 
 
 def ft_part1(data):
-	return prod(find_nbrs(data, 2))
+	''' Get the product of the 2 numbers found. '''
+	return np.prod(ft_find_nbrs(data, 2))
 
 
 def ft_part2(data):
-	return prod(find_nbrs(data, 3))
+	''' Get the product of the 3 numbers found. '''
+	return np.prod(ft_find_nbrs(data, 3))
 
 
 if __name__ == '__main__':
-	ft_core(input_file, ft_input_parser, ft_part1, ft_part2)
+	ft_core(sys.argv[1], ft_input_parser, ft_part1, ft_part2)
